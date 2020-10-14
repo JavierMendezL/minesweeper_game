fafcd702bd54cb8533a2febbba2d3c69c80ec345
kumarbhupi <kumar.bhupi13@gmail.com>
2020-10-14 11:24:06 +0200

    Creating project and uploading to github

---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,10 @@
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class LibraryTest {
+
+  @Test
+  public void testSomeLibraryMethod() {
+    Library classUnderTest = new Library();
+  }
+}
