e0bc6e9018494e5345c09da28f0fd839f41075d2
Sofia <Sofia@LAPTOP-RHAU27CB>
2020-10-20 12:16:21 +0200

    test testsetsimplegrid passed

--- src/main/java/Table.java
+++ src/main/java/Table.java
@@ -1,15 +1,21 @@
 public class Table {
+  //min row i col 8
+  //max rom=24 max col=32
+
+  private int row;
+  private int col;
 
   public Table(int row, int col, int mines){
-
+    this.row=row;
+    this.col=col;
   }
 
   public int getRow(){
-    return 0;
+    return row;
   }
 
   public int getCol(){
-    return 0;
+    return col;
   }
 
 }
