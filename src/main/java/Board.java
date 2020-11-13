395f7875d96d60e93af747d1fcd4440200b7bb98
kumarbhupi <kumar.bhupi13@gmail.com>
2020-11-13 14:03:10 +0100

    testAssignRowsColsToBoard passed

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,12 +1,17 @@
 public class Board {
 
+  private int rows;
+  private int cols;
+
   public void setRows(int i) {
+    this.rows = i;
   }
 
   public void setCols(int i) {
+    this.cols = i;
   }
 
   public int getTotalCellNumber() {
-    return 0;
+    return rows*cols;
   }
 }
--- src/test/java/GridBoxTest.java
+++
@@ -1,9 +0,0 @@
-import junit.framework.TestCase;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-
-public class GridBoxTest extends TestCase{
-
-
-}
