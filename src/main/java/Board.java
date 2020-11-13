6bc24f6d3bfbc39f4d6f50a707d047bc42844331
kumarbhupi <kumar.bhupi13@gmail.com>
2020-11-13 14:38:40 +0100

    testAssignRowsColsToBoardFails: Test to assign negative, null or 0 to rows and cols.

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,13 +1,22 @@
+import java.security.InvalidParameterException;
+
 public class Board {
 
   private int rows;
   private int cols;
+
+  public Board(int rows, int cols) {
+
+
+  }
+
 
   public void setRows(int i) {
     this.rows = i;
   }
 
   public void setCols(int i) {
+    assert i>=0;
     this.cols = i;
   }
 
--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -1,18 +1,44 @@
 import junit.framework.TestCase;
+import org.junit.Before;
 
-public class BoardTest extends TestCase {
+public class BoardTest extends TestCase{
   Board board;
-  public void setUp() throws Exception {
-    super.setUp();
-    board = new Board();
+  @Before
+  public void setUp(){
+    board = new Board(5,5);
   }
 
-  public void testAssignRowsColsToBoard(){
 
+  public void testAssignRowsColsToBoard(){
     board.setRows(5);
     board.setCols(5);
     int totalCellNumber = board.getTotalCellNumber();
     assertEquals(25, totalCellNumber);
 
   }
+
+  public void testAssignRowsColsToBoardFails(){
+    //-1 for error.
+    //Test with negative, null or zero cols/rows
+    Board testBoard= new Board(0,0);
+
+    testBoard.setCols(0);
+    assertEquals(-1, testBoard.getTotalCellNumber());
+
+    testBoard.setRows(4);
+    assertEquals(-1, testBoard.getTotalCellNumber());
+
+    testBoard.setCols(5);
+    testBoard.setRows(-1);
+    assertEquals(-1, testBoard.getTotalCellNumber());
+
+    testBoard.setCols(3);
+    testBoard.setRows(5);
+    assertEquals(15, testBoard.getTotalCellNumber());
+
+  }
+
+
+
+
 }
