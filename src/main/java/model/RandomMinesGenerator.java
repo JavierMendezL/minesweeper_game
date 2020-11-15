507692504a40720e0e87d76156d6ced44fcc9ca1
bhupikmr <kumar.bhupi13@gmail.com>
2020-11-15 23:09:12 +0100

    testSizeGeneratedArray: passed.

--- src/main/java/model/RandomMinesGenerator.java
+++ src/main/java/model/RandomMinesGenerator.java
@@ -1,7 +1,27 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class RandomMinesGenerator {
-    public static int[] generate(int i, int i1, int i2) {
-        return new int[]{1};
+    public static int[] generate(int rows, int cols, int mines) {
+        int[] minesPosition;
+        try {
+            int[] allCells = new int[rows*cols];
+            List<Integer> integerList = new ArrayList<>();
+            for (int i = 0; i < allCells.length; i++) {
+                integerList.add(i);
+            }
+            Collections.shuffle(integerList);
+
+            minesPosition = integerList.stream().mapToInt(i -> i).toArray();
+            return Arrays.copyOfRange(minesPosition, 0, mines);
+
+        } catch (NegativeArraySizeException exception) {
+            return new int[]{-1};
+        }
+
     }
 }
