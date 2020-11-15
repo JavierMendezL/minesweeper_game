a183fab34ad5a9c9cccd84bfdcf66f218927a643
kumarbhupi <kumar.bhupi13@gmail.com>
2020-11-15 13:31:15 +0100

    testIncrementValueOfCell: PASSED

--- src/main/java/model/Cell.java
+++ src/main/java/model/Cell.java
@@ -1,36 +1,50 @@
 package model;
 
-public class Cell{
+public class Cell {
   public static final int MINE = -1;
   public static final int EMPTY = 0;
   public static final int NUMBER = 1;
   public static final int FLAG = 2;
   public static final int DOUBT = 3;
 
-  private CellState state;
 
   private int type;
   private boolean visible;
-  public Cell(CellState state){
-    this.state = state;
-    this.state.setCell(this);
+  private int value;
+
+  public Cell(int type) {
+    this.type = type;
+    if (type == MINE){
+      value = -1;
+    }else {
+      value = 0;
+    }
   }
 
-  public int getValue(){
-    return state.getValue();
-  }
-  public void incrementValue(){
-    state.setValue(1);
+  public int getValue() {
+    return value;
   }
 
-  public Cell (int x){ this.type=x; }
+  public void incrementValue() {
+    if (type == NUMBER) {
+      value += 1;
+    }
+  }
 
-  public int getType() { return type;}
+  public int getType() {
+    return type;
+  }
 
-  public void setType(int type) { this.type=type; }
+  public void setType(int type) {
+    this.type = type;
+  }
 
-  public boolean isVisible() { return visible; }
+  public boolean isVisible() {
+    return visible;
+  }
 
-  public void setVisible(boolean b) {this.visible=b; }
+  public void setVisible(boolean b) {
+    this.visible = b;
+  }
 
 }
