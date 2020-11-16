7f960d5fc8c1bc7d24ec6d22c4f42fa198f0443a
Sofia <Sofia@LAPTOP-RHAU27CB>
2020-11-16 10:47:31 +0100

    class RandomMinesGenerator: testSiezeGeneratedArray: added cases. PASSED.

--- src/test/java/model/RandomMinesGeneratorTest.java
+++ src/test/java/model/RandomMinesGeneratorTest.java
@@ -10,6 +10,13 @@ public class RandomMinesGeneratorTest extends TestCase {
     public void testSizeGeneratedArray(){
         int[] expected = RandomMinesGenerator.generate(10, 8, 3);
         assertEquals(3, expected.length);
+
+        int[] expected1 = RandomMinesGenerator.generate(10, 8, 1);
+        assertEquals(1, expected1.length);
+
+        int[] expected2 = RandomMinesGenerator.generate(10, 8, 5);
+        assertEquals(5, expected2.length);
+
     }
 
     public void testPassedNegativeValueReturnsArrayWithMinusOne(){
