7406ad1a5914b2c1c86c8a15321dcc897c320270
Sofia <Sofia@LAPTOP-RHAU27CB>
2020-11-16 21:22:50 +0100

    Implemented method generate in IRandomMinesGenerator. Created MockGenerate.

--- src/main/java/model/IRandomMinesGenerator.java
+++ src/main/java/model/IRandomMinesGenerator.java
@@ -1,8 +1,28 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public interface IRandomMinesGenerator {
 
-  public static int generate(){
+  public static int[] generate(int rows, int cols, int mines) {
+    int[] minesPosition;
+    try {
+      int[] allCells = new int[rows*cols];
+      if(rows<=0 || cols <= 0){return new int[]{-1};}
+      List<Integer> integerList = new ArrayList<>();
+      for (int i = 0; i < allCells.length; i++) {
+        integerList.add(i);
+      }
+      Collections.shuffle(integerList);
 
-  }
+      minesPosition = integerList.stream().mapToInt(i -> i).toArray();
+      return Arrays.copyOfRange(minesPosition, 0, mines);
+
+    } catch (Exception exception) {
+      return new int[]{-1};
+    }
+
 }
--- src/main/java/model/RandomMinesGenerator.java
+++ src/main/java/model/RandomMinesGenerator.java
@@ -5,24 +5,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class RandomMinesGenerator {
-    public static int[] generate(int rows, int cols, int mines) {
-        int[] minesPosition;
-        try {
-            int[] allCells = new int[rows*cols];
-            if(rows<=0 || cols <= 0){return new int[]{-1};}
-            List<Integer> integerList = new ArrayList<>();
-            for (int i = 0; i < allCells.length; i++) {
-                integerList.add(i);
-            }
-            Collections.shuffle(integerList);
+public class RandomMinesGenerator implements IRandomMinesGenerator{
 
-            minesPosition = integerList.stream().mapToInt(i -> i).toArray();
-            return Arrays.copyOfRange(minesPosition, 0, mines);
-
-        } catch (Exception exception) {
-            return new int[]{-1};
-        }
-
-    }
 }
